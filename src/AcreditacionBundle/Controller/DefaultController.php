9f729e6bee0a38ee688e565a6a455fc5057d8e33
Gerardo Melendez <gmelendez@uca.edu.sv>
2016-09-11 03:45:00 +0000

    Maquetado (hecho por José)

--- src/AcreditacionBundle/Controller/DefaultController.php
+++ src/AcreditacionBundle/Controller/DefaultController.php
@@ -10,4 +10,30 @@ class DefaultController extends Controller
     {
         return $this->render('AcreditacionBundle:Default:index.html.twig');
     }
+    
+    
+    
+    public function recoveryAction()
+    {
+        return $this->render('default/recovery.index.html.twig');
+    }
+    
+    public function adminAction()
+    {
+        return $this->render('default/admin.index.html.twig');
+    }
+    
+    public function inicioAction()
+    {
+        return $this->render('default/inicio.index.html.twig');
+    }
+    public function adduserAction()
+    {
+        return $this->render('default/adduser.index.html.twig');
+    }
+     public function listauserAction()
+    {
+        return $this->render('default/listauser.index.html.twig');
+    }
+    
 }
